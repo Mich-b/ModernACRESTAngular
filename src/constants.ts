b6952d4ff837233bbb0209a47cc4d663aac7a0ea
michael.boeynaems@portasecura.com <mboeynaems@outlook.com>
2019-11-21 21:21:32 +0100

    fix vulnerabilities

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 export class Constants {
-  public static stsAuthority = 'https://cevora.eu.auth0.com/';
-  public static clientId = '4kaf31F1XS0EX4OqEA3pcqnOLBo8pq9h';
-  public static clientRoot = 'https://gracious-kare-74625e.netlify.com/';
+  public static stsAuthority = 'https://authncourse.eu.auth0.com/';
+  public static clientId = 'xx0qugSvBSo83qlDBMCNidyMMuCGVXpl';
+  public static clientRoot = 'http://localhost:4200/';
+  //public static clientRoot = 'https://gracious-kare-74625e.netlify.com/';
   public static clientScope = 'openid profile email read';
   public static apiAudience = 'https://localapi.internal';
   public static apiRoot = 'https://demo.identityserver.io/api/';
